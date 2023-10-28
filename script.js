5cabf6d2e2d9dd7dc1c8d25fb68e252a9d791b48
MuhammadAnsub090 <ansubwasi058@gmail.com>
2023-10-28 12:20:10 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+
+let arr = ["red", "blue", "green", "orange", "black"];
+
+function button(color) {
+  if (color === "red") {
+   
+    document.body.style.backgroundColor = "red";
+  } else if (color === "blue") {
+    
+    document.body.style.backgroundColor = "blue";
+  } else if (color === "green") {
+    
+    document.body.style.backgroundColor = "green";
+  } else if (color === "orange") {
+    
+    document.body.style.backgroundColor = "orange";
+  } else if (color === "purple") {
+    
+    document.body.style.backgroundColor = "purple";
+  } else if (color === "grey") {
+    
+    document.body.style.backgroundColor = "grey";
+  } else if (color === "pink") {
+
+    document.body.style.backgroundColor = "pink"; 
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
